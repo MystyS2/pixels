153fa0034fd0ef4f075e991a426f0b6f0d0c0259
MiHyeon <kkmmhh7478@gmail.com>
2024-09-23 18:39:58 +0900

    홈 화면 높이 수정

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const HomePage = () => {
   return (
-    <div className='bg-black w-full h-fit min-h-[866px] flex justify-center'>
+    <div className='bg-black w-full h-[calc(100vh-64px)] flex justify-center'>
       <img src='https://pixelnetwork.kr/static/images/pixel_logo_white.svg' alt='' className='h-36 top-1/3 absolute' />
     </div>
   )
