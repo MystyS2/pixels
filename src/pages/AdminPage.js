c6dd463a4a7407f7a429e8d795aae1bbc2db2fcf
MiHyeon <kkmmhh7478@gmail.com>
2024-09-26 21:22:53 +0900

    폼 구성 내용 추가

--- src/pages/AdminPage.js
+++ src/pages/AdminPage.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Input, Button, Card } from "@nextui-org/react"; // NextUI 컴포넌트
+import { Input, Button, Card } from "@nextui-org/react";
 import {
   addCreator,
   updateCreator,
@@ -9,10 +9,17 @@ import {
 
 const AdminPage = () => {
   const [creators, setCreators] = useState([]);
-  const [name, setName] = useState("");
-  const [description, setDescription] = useState("");
-  const [twitter, setTwitter] = useState("");
-  const [instagram, setInstagram] = useState("");
+  const [creatorData, setCreatorData] = useState({
+    name: "",
+    birthday: "",
+    youtube: "",
+    chzzk: "",
+    africatv: "",
+    twitter: "",
+    instagram: "",
+    cafe: "",
+    modelPath: "",
+  });
   const [selectedCreator, setSelectedCreator] = useState(null);
 
   useEffect(() => {
@@ -21,21 +28,20 @@ const AdminPage = () => {
     });
   }, []);
 
-  const handleSave = async () => {
-    const newCreator = {
-      name,
-      description,
-      socials: {
-        twitter,
-        instagram,
-      },
-    };
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setCreatorData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
 
+  const handleSave = async () => {
     if (selectedCreator) {
-      await updateCreator(selectedCreator.id, newCreator);
+      await updateCreator(selectedCreator.id, creatorData);
       alert("크리에이터 정보가 수정되었습니다.");
     } else {
-      await addCreator(newCreator);
+      await addCreator(creatorData);
       alert("새 크리에이터가 추가되었습니다.");
     }
 
@@ -57,17 +63,33 @@ const AdminPage = () => {
 
   const handleEdit = (creator) => {
     setSelectedCreator(creator);
-    setName(creator.name);
-    setDescription(creator.description);
-    setTwitter(creator.socials.twitter);
-    setInstagram(creator.socials.instagram);
+
+    // creatorData의 구조에 맞게 필드 할당
+    setCreatorData({
+      name: creator.name,
+      birthday: creator.birthday,
+      youtube: creator.platforms.youtube,
+      chzzk: creator.platforms.chzzk,
+      africatv: creator.platforms.africatv,
+      twitter: creator.socials.twitter,
+      instagram: creator.socials.instagram,
+      cafe: creator.socials.cafe,
+      modelPath: creator.modelPath
+    });
   };
 
   const resetForm = () => {
-    setName("");
-    setDescription("");
-    setTwitter("");
-    setInstagram("");
+    setCreatorData({
+      name: "",
+      birthday: "",
+      youtube: "",
+      chzzk: "",
+      africatv: "",
+      twitter: "",
+      instagram: "",
+      cafe: "",
+      modelPath: ""
+    });
     setSelectedCreator(null);
   };
 
@@ -76,22 +98,26 @@ const AdminPage = () => {
       <h1 className="text-3xl font-bold mb-6">크리에이터 추가 및 수정</h1>
 
       <Card className="mb-8 p-6" shadow={false} bordered>
-        <h2 className="text-xl font-bold mb-4">{selectedCreator ? "크리에이터 수정" : "크리에이터 추가"}</h2>
+        <h2 className="text-xl font-bold mb-4">
+          {selectedCreator ? "크리에이터 수정" : "크리에이터 추가"}
+        </h2>
 
         <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 mb-4">
           <Input
             label="이름"
             placeholder="크리에이터 이름을 입력하세요"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
+            name="name"
+            value={creatorData.name}
+            onChange={handleInputChange}
             clearable
             bordered
           />
           <Input
-            label="설명"
-            placeholder="크리에이터 설명을 입력하세요"
-            value={description}
-            onChange={(e) => setDescription(e.target.value)}
+            label="유튜브"
+            placeholder="유튜브 링크"
+            name="youtube"
+            value={creatorData.youtube}
+            onChange={handleInputChange}
             clearable
             bordered
           />
@@ -99,18 +125,62 @@ const AdminPage = () => {
 
         <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 mb-4">
           <Input
-            label="Twitter"
-            placeholder="Twitter URL 입력"
-            value={twitter}
-            onChange={(e) => setTwitter(e.target.value)}
+            label="치지직"
+            placeholder="치지직 링크"
+            name="chzzk"
+            value={creatorData.chzzk}
+            onChange={handleInputChange}
+            clearable
+            bordered
+          />
+          <Input
+            label="아프리카tv"
+            placeholder="아프리카 링크"
+            name="africatv"
+            value={creatorData.africatv}
+            onChange={handleInputChange}
+            clearable
+            bordered
+          />
+        </div>
+
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 mb-4">
+          <Input
+            label="생일"
+            placeholder="생일"
+            name="birthday"
+            value={creatorData.birthday}
+            onChange={handleInputChange}
             clearable
             bordered
           />
           <Input
             label="Instagram"
             placeholder="Instagram URL 입력"
-            value={instagram}
-            onChange={(e) => setInstagram(e.target.value)}
+            name="instagram"
+            value={creatorData.instagram}
+            onChange={handleInputChange}
+            clearable
+            bordered
+          />
+        </div>
+
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 mb-4">
+          <Input
+            label="팬카페"
+            placeholder="팬카페 링크"
+            name="cafe"
+            value={creatorData.cafe}
+            onChange={handleInputChange}
+            clearable
+            bordered
+          />
+          <Input
+            label="3d 모델 파일명"
+            placeholder="3d 모델 경로"
+            name="modelPath"
+            value={creatorData.modelPath}
+            onChange={handleInputChange}
             clearable
             bordered
           />
@@ -132,7 +202,10 @@ const AdminPage = () => {
 
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
         {creators.map((creator) => (
-          <div key={creator.id} className="bg-[#18181B] shadow-lg rounded-lg p-4">
+          <div
+            key={creator.id}
+            className="bg-[#18181B] shadow-lg rounded-lg p-4"
+          >
             <h3 className="font-semibold">{creator.name}</h3>
             <p>{creator.description}</p>
             <div className="flex justify-between mt-4">
